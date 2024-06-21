ae0e2ce926a95cc0d5f194403e22d4dbba87b061
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2024-06-21 05:59:07 +0300

    Test for babel loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,5 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    mode: 'development',
+    devtool: 'source-map',
+    stats: 'verbose',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            }
+        ]
+    }
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,5 +2,15 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/client/index.js'
+    entry: './src/client/index.js',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            }
+        ]
+    }
 }
