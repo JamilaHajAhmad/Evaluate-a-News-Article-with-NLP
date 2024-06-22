6a2f7dd2180d168b8fdd336302f670ccc0370a31
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2024-06-22 21:26:37 +0300

    Update clean webpack plugin and Add new rule to module for SASS files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,29 @@
 const path = require('path');
 const webpack = require('webpack');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -19,7 +32,7 @@ module.exports = {
             template: './src/client/views/index.html',
             filename: './index.html'
         }),
-        new cleanWebpackPlugin({
+        new CleanWebpackPlugin({
             // Simulate the removal of files
             dry: true,
             // Write Logs to the console
