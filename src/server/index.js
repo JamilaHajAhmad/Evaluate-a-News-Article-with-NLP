5a3715e4212690827df20e1fd329d62f069bcf5a
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2024-06-22 12:08:48 +0300

    POST and GET routes with API configuration

--- src/server/index.js
+++ src/server/index.js
@@ -8,3 +8,25 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('dist'));
+let meaningCloud = new meaningCloud({
+    application_key: process.env.API_KEY
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('dist/index.html');
+});
+
+app.post('/analyze', (req, res) => {
+    meaningCloud.sentiment(req.body.text, (err, response) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send('https://api.meaningcloud.com/sentiment-2.1');
+        }
+    });
+});
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000`);
+});
