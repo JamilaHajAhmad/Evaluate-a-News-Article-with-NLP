a240fafcbcfd97ac1fd504ec0cbdf878a72481c3
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2024-06-22 09:59:13 +0300

    index.js server file configuration

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
