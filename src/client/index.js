de19c7ba332092953e958ff2b29f86b183ecb485
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2024-06-22 21:54:13 +0300

    index.js file from client app

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,12 @@ import './styles/base.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
-import './styles/resets.scss';
 
+import { handleSubmit } from './js/formHandler';
+import { urlChecker } from './js/urlChecker';
 alert('Im Here :)');
+
+export {
+    handleSubmit,
+    urlChecker
+}
